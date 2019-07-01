3c32a27145b5774e7311d7c23f7d49a387739811
Marcos Casor <marcosicp2012@gmail.com>
2019-07-01 04:24:32 +0000

    urls.config.ts edited

--- src/app/shared/configs/urls.config.ts
+++ src/app/shared/configs/urls.config.ts
@@ -1,6 +1,6 @@
 import BaseConfig from './base.config';
 
-const isProd = false,
+const isProd = true,
       BaseUrl = isProd ? BaseConfig.prodUrl : BaseConfig.localUrl,
       UserUrl = {
         home: 'usuarios',
