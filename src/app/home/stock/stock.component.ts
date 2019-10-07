4f3cea1bc8b4d3c51edc85b235c1522306364927
Nicolas Omar Gonzalez Passerino <nicolas.passerino@gmail.com>
2019-10-07 00:58:03 -0300

    Se agregaron botones para agregar, modificar y elminar productos, como tambien su stock. Dialogos de stock creados. Cambio del componente Stock de lugar. Arreglo del simbolo a pesos en las tablas

---
+++ src/app/home/stock/stock.component.ts
@@ -0,0 +1,109 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatDialog, MatButtonToggle } from '@angular/material';
+// ENTIDADES
+import { Productos } from '../../shared/models/producto.model';
+// SERVICIOS
+import { DataService } from '../../core/services/data.service';
+// DIALOGOS
+import { DialogStockAddEditComponent } from '../../dialogs/dialog-stock-add-edit/dialog-stock-add-edit.component';
+import { DialogStockAumentarComponent } from '../../dialogs/dialog-stock-aumentar/dialog-stock-aumentar.component';
+import { DialogConfirmarComponent } from '../../dialogs/dialog-confirmar/dialog-confirmar.component';
+// CONFIGURACIONES
+import { URL_PRODUCTOS } from '../../shared/configs/urls.config';
+import { TABLA_STOCK } from '../../shared/configs/table.config';
+
+@Component({
+  selector: 'stock',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.scss']
+})
+export class StockComponent implements OnInit {
+  tableTitle = TABLA_STOCK.title;
+  dataSource = new MatTableDataSource<Productos>();
+  headerTitles = Object.keys(TABLA_STOCK.cells);
+  tableHeaders = TABLA_STOCK.headers;
+  columnCells = TABLA_STOCK.cells;
+  formatTableCells = TABLA_STOCK.format;
+  isLoading: boolean;
+  addButton = {
+    label: 'Agregar producto',
+    buttonEvent: () => this.agregarProducto()
+  };
+
+  constructor(
+    private dataService: DataService,
+    public dialog: MatDialog
+  ) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.dataService.getAsync(URL_PRODUCTOS.GET_ALL, []).subscribe(
+      data => {
+        this.dataSource.data = data;
+        this.columnCells.opciones = [{
+          buttonLabel: 'Modificar',
+          buttonEvent: (prod) => this.editarProducto(prod)
+        }, {
+          buttonLabel: 'Agregar stock',
+          buttonEvent: (prod) => this.cambiarStock(prod)
+        }, {
+          buttonLabel: 'Eliminar',
+          buttonEvent: (prod) => this.eliminarProducto(prod)
+        }];
+        this.isLoading = false;
+      },
+      error => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  agregarProducto() {
+    this.dialog.open(
+      DialogStockAddEditComponent, {
+        width: '900px'
+      }
+    );
+  }
+
+  editarProducto(prod: Productos) {
+    this.dialog.open(
+      DialogStockAddEditComponent, {
+        width: '900px',
+        data: prod
+      }
+    );
+  }
+
+  cambiarStock(prod: Productos) {
+    this.dialog.open(
+      DialogStockAumentarComponent, {
+        width: '900px',
+        data: prod
+      }
+    );
+  }
+
+  eliminarProducto(prod: Productos) {
+    const dialogRef = this.dialog.open(
+      DialogConfirmarComponent,
+      {
+        width: '900px',
+        data: {
+          title: 'Eliminar producto',
+          confirmText: 'Esta seguro que desea eliminar este producto?'
+        }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.confirm) {
+        this.dataService.deleteAsync(URL_PRODUCTOS.DELETE_STOCK, prod.id, []).subscribe(
+          data => {
+              this.dataSource.data = data;
+              this.isLoading = false;
+          }
+        );
+      }
+    });
+  }
+}
