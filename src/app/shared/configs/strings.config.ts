f697c7aaff9e2c18bb7424af7e86d396dca21384
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2019-10-14 13:59:43 -0300

    Se modifica el login con mejoras de interaccion y validacion. Se actualizan iconos de la pantalla principal con los del menu lateral

--- src/app/shared/configs/strings.config.ts
+++ src/app/shared/configs/strings.config.ts
@@ -1,6 +1,9 @@
 const strings = {
   badLogin: 'Usuario y/o constraseña inválidos',
-  noInternet: 'Hubo un problema de conexion. Pruebe de conectarse en unos minutos-'
+  noInternet: 'Hubo un problema de conexion. Pruebe de conectarse en unos minutos',
+  regex: {
+    email: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  }
 };
 
 export default strings;
