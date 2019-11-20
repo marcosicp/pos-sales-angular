b5b26cfbcd18772797b70ac04ef08d1c9ca203ac
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2019-11-20 04:12:12 -0300

    Primer implementacion de reportes usando d3 para mostrar el historicos de las ventas

--- src/app/home/reportes/reportes.component.ts
+++ src/app/home/reportes/reportes.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { DataService } from '../../core/services/data.service';
+import { URL_VENTAS } from '../../shared/configs/urls.config';
+import { DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
-  styleUrls: ['./reportes.component.scss']
+  styleUrls: ['./reportes.component.scss'],
+  providers: [DatePipe, CurrencyPipe]
 })
 export class ReportesComponent implements OnInit {
+  data = [10, 20, 30];
 
-  constructor() { }
+  constructor(
+    private dataService: DataService,
+    private datePipe: DatePipe,
+    private currencyPipe: CurrencyPipe
+  ) { }
 
   ngOnInit() {
+    this.dataService.getAsync(URL_VENTAS.GET_ALL, []).subscribe(
+      ventas => {
+        d3.select('.chart')
+        .selectAll('div')
+        .data(ventas)
+          .enter()
+          .append('div')
+          .style('width', (v: any) => `${v.pedido.total / 5}px`)
+          .style('background-color', () => 'steelblue')
+          .style('color', () => 'white')
+          .style('padding', () => '3px')
+          .style('margin', () => '3px')
+          .text((v: any) => `${this.datePipe.transform(v.creado, 'short')}  -  Total: ${this.currencyPipe.transform(v.pedido.total, '$')}`);
+      }
+    );
+
+
   }
 
 }
